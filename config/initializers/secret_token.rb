97a144cd50e6b802e52f1e1ce345c73adb2e35f0
Rich Bowman <BostonREB99@gmail.com>
2014-08-26 21:09:52 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Football::Application.config.secret_key_base = '786b9963ba482817e049fb1a1ec822cb31949d6e7ef5607b1518d0c73ff08f889588f45e2f34179d1587ca28c51b73fee61ec255664a3eb3a3be31c35518ffe0'
