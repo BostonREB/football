2fabb286dee8b0813cc6b2bc45693883c9fe8c2d
Rich Bowman <BostonREB99@gmail.com>
2014-09-29 13:50:03 -0400

    revise player selection methods

--- app/controllers/team_player_relationships_controller.rb
+++ app/controllers/team_player_relationships_controller.rb
@@ -1,17 +1,18 @@
 class TeamPlayerRelationshipsController < ApplicationController
 
   def create
-    team = current_user.team
-    @player = Player.find(params[:id])
-    team_player_relationship = team.pick(@player)
-    render :change
+    @team = Team.find(params[:id])
+    @player = Player.find(params[:player_id])
+    team_player_relationship = TeamPlayerRelationship.create(selecting_team: @team, selected_player: @player)
+    redirect_to team_path(@team)
   end
 
   def destroy
-    team = current_user.team
-    @player = Player.find(params[:id])
-    team.drop(@player)
-    render :change
+    @team = Team.find(params[:id])
+    @player = Player.find(params[:player_id])
+    team_player_relationship = @team.team_player_relationships.where(selected_player: @player)
+    team_player_relationship.destroy_all
+    redirect_to team_path(@team)
   end
 
 end
